2933c08c060d7322d912a1e93d2d69c6da0ea740
viktor hugo <victorHbenitez85@hotmail.com>
2017-09-26 04:32:32 -0500

    Add clickListenerEvent

--- app/src/main/java/com/example/victorbenitez/holamundo/MainActivity.java
+++ app/src/main/java/com/example/victorbenitez/holamundo/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.victorbenitez.holamundo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,34 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        // GET THE VIEWS FROM content_main.xml
+        // 1. Get el tipo de View que deseamos obtener (TextView)
+        // R : Archivo que contiene todos los ID que vayamos creando
+
+        // Obteniendo un View de tipo TextView y lo esa guardando en un objeto de tipo TextView que se llama answerText
+        final TextView answerText = (TextView) findViewById(R.id.answer_text);
+
+        // Agregar propiedades desde MainActivity java file
+        answerText.setText("Aqui puedes ver el resultado sobre la edad perruna de tu mascota");
+
+        // GET THE IMAGEVIEW FROM content_main.xml
+        ImageView coverImage = (ImageView) findViewById(R.id.cover_image);
+        // set the image to the ImageView  ContextCompat.getDrawable(contexto, AccedemosRecursoArchivo)
+        coverImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dog));
+
+        // GET THE BUTTON VIEW AND ADD EVENT
+        Button mainButton = (Button) findViewById(R.id.main_button);
+
+        // call this method when the user pressed the button
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answerText.setText("ACABAS DE PRESIONAR EL BOTON YA ERES PROGRAMADOR");
+            }
+        });
+
     }
 
     @Override
