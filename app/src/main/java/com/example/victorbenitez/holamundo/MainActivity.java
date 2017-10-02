006613f4ea282e58ad306db48ca0278a1b76660c
viktor hugo <victorHbenitez85@hotmail.com>
2017-10-02 02:18:12 -0500

    Terminada Aplicacion Edad Canina

--- app/src/main/java/com/example/victorbenitez/holamundo/MainActivity.java
+++ app/src/main/java/com/example/victorbenitez/holamundo/MainActivity.java
@@ -6,14 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();  // Toma el nombre de la clase manejada
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +36,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         // GET THE VIEWS FROM content_main.xml
         // 1. Get el tipo de View que deseamos obtener (TextView)
         // R : Archivo que contiene todos los ID que vayamos creando
 
         // Obteniendo un View de tipo TextView y lo esa guardando en un objeto de tipo TextView que se llama answerText
         final TextView answerText = (TextView) findViewById(R.id.answer_text);
-
-        // Agregar propiedades desde MainActivity java file
-        answerText.setText("Aqui puedes ver el resultado sobre la edad perruna de tu mascota");
-
-        // GET THE IMAGEVIEW FROM content_main.xml
-        ImageView coverImage = (ImageView) findViewById(R.id.cover_image);
-        // set the image to the ImageView  ContextCompat.getDrawable(contexto, AccedemosRecursoArchivo)
-        coverImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.dog));
-
+        // final : define variables constantes
+        final EditText ageEdit = (EditText) findViewById(R.id.age_edit);
         // GET THE BUTTON VIEW AND ADD EVENT
         Button mainButton = (Button) findViewById(R.id.main_button);
 
@@ -54,10 +51,20 @@ public class MainActivity extends AppCompatActivity {
         mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                answerText.setText("ACABAS DE PRESIONAR EL BOTON YA ERES PROGRAMADOR");
+
+                String ageString = ageEdit.getText().toString(); // get value: string
+
+                try {
+                    int ageInt = Integer.parseInt(ageString); // cat the value to int
+                    String res = getString(R.string.result);
+                    answerText.setText(String.format(res, (ageInt * 6))); // mostar un resultado en un string declarado en string.xml
+
+                }catch (NumberFormatException e){
+                   //answerText.setText("NumberFormatException por favor introduce un numero entero");
+                    Toast.makeText(MainActivity.this, getString(R.string.error_message), Toast.LENGTH_LONG).show();
+                }
             }
         });
-
     }
 
     @Override
